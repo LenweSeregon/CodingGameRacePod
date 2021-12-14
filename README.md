0fb2c063afab657d78dcee5915b81d49a710e7d3
Nicolas Serf <serf.nicolas@gmail.com>
2021-12-14 16:47:04 +0100

    Base code architecture + readme update

--- README.md
+++ README.md
@@ -6,9 +6,15 @@
 The game bring to wood 2 by following the step by step tutorial, correcting the tiny error, and modifying the trust value so I'll skip that part and directly begin  explaination and reasoning at Wood 2.
 
 ### From Wood 2
+* My first step as stated in 14/12/2012 was to work on my base code architecture in order to make the code evolve faster afterwards
 
 ## Timeline
 
 ### 13/12/2012 : (~20 min)
 * Create the git repository and Readme structure
 * Start the codingame project and reached Wood 2 following the tutorial
+
+### 14/12/2012 : (~1h20)
+* I've been working on the code architecture, creating various classes as "Pod", "Checkpoint", "Strategy", etc... in order to continue with a clean and evolutive base
+* Ensuring that the project is still working as expected, testing the code but without working on beating the Wood 2 boss for the moment
+* Some research on C++ features I had forgotten or never really work with like pointer to member-function
---
+++ main.cpp
@@ -0,0 +1,286 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+// ===================================
+// ==== Classes forward declaration
+// ===================================
+
+class Pod;
+class PodStrategy;
+
+// ===================================
+// ==== Utility classes
+// ===================================
+
+template<typename T, typename C>
+class Property
+{
+	using SetterType = void (C::*)(T);
+	using GetterType = T(C::*)() const;
+
+private:
+
+	C* const _mObject;
+	SetterType const _mSetter;
+	GetterType const _mGetter;
+
+public:
+
+	Property(C* aObject, SetterType aSetter, GetterType aGetter) :
+		_mObject(aObject),
+		_mSetter(aSetter),
+		_mGetter(aGetter)
+	{
+
+	}
+
+	operator T() const
+	{
+		return (_mObject->*_mGetter)();
+	}
+
+	C& operator = (T aValue)
+	{
+		(_mObject->*_mSetter)(aValue);
+		return *_mObject;
+	}
+};
+
+template <typename T>
+class Vector2
+{
+	static_assert(std::is_same<T, double>::value || std::is_same<T, float>::value || std::is_same<T, int>::value, "Type is not valid");
+
+private:
+
+	T _mX;
+	T _mY;
+
+	T GetX() const
+	{
+		return _mX;
+	}
+
+	T GetY() const
+	{
+		return _mY;
+	}
+
+	void SetX(int aValue)
+	{
+		_mX = aValue;
+	}
+
+	void SetY(int aValue)
+	{
+		_mY = aValue;
+	}
+
+public:
+
+	Property<T, Vector2> X{ this, &Vector2::SetX, &Vector2::GetX };
+	Property<T, Vector2> Y{ this, &Vector2::SetY, &Vector2::GetY };
+
+	Vector2(T aX, T aY) :
+		_mX(aX),
+		_mY(aY)
+	{
+
+	}
+
+	Vector2<T>(Vector2<T> const&) = default;
+
+	Vector2<T>& operator=(Vector2<T> other)
+	{
+		_mX = other.X;
+		_mY = other.Y;
+		return *this;
+	}
+};
+
+class Entity
+{
+
+protected:
+
+	Vector2<int> _mPosition;
+
+public:
+
+	Entity(Vector2<int> aPosition) :
+		_mPosition(aPosition)
+	{
+
+	}
+
+	Vector2<int> GetPosition() const
+	{
+		return _mPosition;
+	}
+};
+
+
+class Checkpoint : public Entity
+{
+private:
+
+	int _mAngle;
+	int _mDistance;
+
+public:
+
+	Checkpoint(Vector2<int> aPosition, int aAngle, int aDistance) :
+		Entity(aPosition),
+		_mAngle(aAngle),
+		_mDistance(aDistance)
+	{
+
+	}
+};
+
+class Pod : public Entity
+{
+private:
+
+	int _mThrust;
+	const PodStrategy* _mStrategy;
+
+public:
+
+	Pod(Vector2<int> aPosition, int aThrust) :
+		Entity(aPosition),
+		_mThrust(aThrust)
+	{
+
+	}
+
+	void UpdatePosition(Vector2<int> aPosition);
+	void UpdateStrategy(const PodStrategy& aStrategy);
+
+	Vector2<int> Move(const Checkpoint& aNextCheckpoint) const;
+	int Thrust(const Checkpoint& aNextCheckpoint) const;
+
+};
+
+
+class PodStrategy
+{
+
+public:
+
+	PodStrategy()
+	{
+
+	}
+
+	virtual Vector2<int> ComputeNextPosition(const Pod& pod, const Checkpoint& aNextCheckpoint) const = 0;
+	virtual int ComputeNextThrust(const Pod& pod, const Checkpoint& aNextCheckpoint) const = 0;
+};
+
+class StandardPodStrategy : public PodStrategy
+{
+public:
+
+	StandardPodStrategy() :
+		PodStrategy()
+	{
+
+	}
+
+	Vector2<int> ComputeNextPosition(const Pod& pod, const Checkpoint& aNextCheckpoint) const;
+	int ComputeNextThrust(const Pod& pod, const Checkpoint& aNextCheckpoint) const;
+};
+
+// ===================================
+// ==== Inline implementations
+// ===================================
+
+// ==== Pod ====
+inline void Pod::UpdatePosition(Vector2<int> aPosition)
+{
+	_mPosition = aPosition;
+}
+
+inline void Pod::UpdateStrategy(const PodStrategy& aStrategy)
+{
+	_mStrategy = &aStrategy;
+}
+
+inline Vector2<int> Pod::Move(const Checkpoint &aNextCheckpoint) const
+{
+	return _mStrategy->ComputeNextPosition(*this, aNextCheckpoint);
+}
+
+inline int Pod::Thrust(const Checkpoint& aNextCheckpoint) const
+{
+	return _mStrategy->ComputeNextThrust(*this, aNextCheckpoint);
+}
+
+// ==== Standard Pod Strategy ====
+inline Vector2<int> StandardPodStrategy::ComputeNextPosition(const Pod& pod, const Checkpoint& aNextCheckpoint) const
+{
+	return { aNextCheckpoint.GetPosition().X, aNextCheckpoint.GetPosition().Y };
+}
+
+inline int StandardPodStrategy::ComputeNextThrust(const Pod& pod, const Checkpoint& aNextCheckpoint) const
+{
+	return 100;
+}
+
+// ===================================
+// ==== Main function
+// ===================================
+int main()
+{
+
+	Pod pod{ Vector2{0, 0}, 50 };
+	StandardPodStrategy strategy;
+
+	pod.UpdateStrategy(strategy);
+
+	while (1) {
+
+		// ============================
+		// == Retrieving data from the standard input
+		// ============================
+
+		int x;
+		int y;
+		int next_checkpoint_x; // x position of the next check point
+		int next_checkpoint_y; // y position of the next check point
+		int next_checkpoint_dist; // distance to the next checkpoint
+		int next_checkpoint_angle; // angle between your pod orientation and the direction of the next checkpoint
+		std::cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; std::cin.ignore();
+		int opponent_x;
+		int opponent_y;
+		std::cin >> opponent_x >> opponent_y; std::cin.ignore();
+
+		// ============================
+		// == Building variables and populating classes
+		// ============================
+
+		Checkpoint nextCheckpoint{ Vector2{next_checkpoint_x, next_checkpoint_y}, next_checkpoint_angle, next_checkpoint_dist };
+
+		// ============================
+		// == Update and computation
+		pod.UpdatePosition({ x, y });
+
+		Vector2<int> computedPosition = pod.Move(nextCheckpoint);
+		int computedThrust = pod.Thrust(nextCheckpoint);
+		// ============================
+
+		// Write an action using cout. DON'T FORGET THE "<< endl"
+		// To debug: cerr << "Debug messages..." << endl;
+
+
+
+
+		// You have to output the target position
+		// followed by the power (0 <= thrust <= 100)
+		// i.e.: "x y thrust"
+		std::cout << computedPosition.X << " " << computedPosition.Y << " " << computedThrust << std::endl;
+	}
+}
